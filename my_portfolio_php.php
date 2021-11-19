2722f868c57576253b074ea2e201c6536838d175
Sajjad Hosen <91114033+sajjad126@users.noreply.github.com>
2021-11-19 22:32:54 +0600

    Update my_portfolio_php.php

--- my_portfolio_php.php
+++ my_portfolio_php.php
@@ -1,24 +1,5 @@
 <?php 
 
-// $name = $_POST['name'];
-// $email = $_POST['email'];
-// $subject = $_POST['subject'];
-// $message = $_POST['message'];
-
-// $name = $_POST['name'];
-// $email= $_POST['email'];
-// $message= $_POST['message'];
-// $to = "youremail@mail.com";
-// $subject = "Mail From website";
-// $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-// $headers = "From: noreply@yoursite.com" . "\r\n" .
-// "CC: somebodyelse@example.com";
-// if($email!=NULL){
-//     mail($to,$subject,$txt,$headers);
-// }
-// //redirect
-// header("Location:thankyou.html");
-
 
 $to_email = "mdsujon5036@gmail.com";
 $name = $_POST['name'];
